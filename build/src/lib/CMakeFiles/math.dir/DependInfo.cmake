3a7ba6d20021716acd24c308c0cc6ae89f9df9c8
wjw <1194792378@qq.com>
2023-03-16 15:32:00 +0800

    add some cpp source and header
    Signed-off-by: wjw <1194792378@qq.com>

---
+++ build/src/lib/CMakeFiles/math.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Users/wujiewen/Desktop/wjw/cmake/template/cmake_test/src/lib/math.cpp" "src/lib/CMakeFiles/math.dir/math.cpp.obj" "gcc" "src/lib/CMakeFiles/math.dir/math.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/lib/math.h
@@ -0,0 +1,9 @@
+#ifndef __MATH_H
+#define __MATH_H
+
+namespace math
+{
+    int add(int a, int b);
+}
+
+#endif
